3910584d47c261339f8dac01a51226327f82a69f
Mohan Gupta <mohangupta@live.com>
2025-10-29 19:02:49 +1100

    deserialize toml

--- src/lib/config/parse.rs
+++ src/lib/config/parse.rs
@@ -1,8 +1,9 @@
 use std::{fs::read_to_string, path::PathBuf};
-
-
+use crate::config::espforge::{EspForgeConfig, EspConfig};
 
 pub fn parse_config(espforge_config: &PathBuf) {
     let contents = read_to_string(&espforge_config).expect("file corrupt");
-    print!("{}\n", contents);
+    let config: EspForgeConfig = toml::from_str(&contents).expect("toml deserial failed");
+    let esp_config: EspConfig = config.espforge;
+    print!("{:?}\n", &esp_config);
 }
