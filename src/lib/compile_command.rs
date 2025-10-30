2fba323a388d31bc1717c173775db682673c51f3
Mohan Gupta <mohangupta@live.com>
2025-10-30 11:48:52 +1100

    todo: need to render correct example

--- src/lib/compile_command.rs
+++ src/lib/compile_command.rs
@@ -1,10 +1,8 @@
 use std::path::PathBuf;
-
 use crate::{config::parse::parse_config, core::generate::generate};
-
 
 pub fn compile(path: &PathBuf) {
     let espforge_config = parse_config(path);
     generate(espforge_config);
-    
+        
 }
