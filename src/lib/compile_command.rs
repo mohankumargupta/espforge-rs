3d0d9b22b63ae9ce01196cc5ec62f46d028fdab6
Mohan Gupta <mohangupta@live.com>
2025-10-30 16:29:22 +1100

    empty config.rs

--- src/lib/compile_command.rs
+++ src/lib/compile_command.rs
@@ -4,5 +4,5 @@ use crate::{config::parse::parse_config, core::generate::generate};
 pub fn compile(path: &PathBuf) {
     let espforge_config = parse_config(path);
     generate(espforge_config);
-        
+    
 }
