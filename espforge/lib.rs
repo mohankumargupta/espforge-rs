81be4d666cb88298778b9dc5f80758e2283de538
Mohan Gupta <mohangupta@live.com>
2025-10-31 13:35:27 +1100

    add Example trait

--- espforge/lib.rs
+++ espforge/lib.rs
@@ -1,3 +1,8 @@
 pub mod compile_command;
 pub mod config;
 pub mod core;
+
+pub trait Example {
+    /// Renders the Askama template for the example.
+    fn render(&self) -> Result<String, askama::Error>;
+}
